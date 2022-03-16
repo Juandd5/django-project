c441596ee4f941540723c32bd5237553c381e6f4
Juan David Díaz <juandiazr95@gmail.com>
2022-03-15 22:10:38 -0500

    Registering models and applying migrations

--- assetsapp/admin.py
+++ assetsapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Person, Area, Asset
 
-# Register your models here.
+
+admin.site.register(Person)
+admin.site.register(Area)
+admin.site.register(Asset)
