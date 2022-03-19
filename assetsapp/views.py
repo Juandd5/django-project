58ac747a8f2b7a5120284497def030827b97fb78
Juan David Díaz <juandiazr95@gmail.com>
2022-03-18 19:54:38 -0500

    Listing all assets with AssetView()

--- assetsapp/views.py
+++ assetsapp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView
+
+from .models import Asset
 
 
 def index(request):
     return render(request, 'assetsapp/index.html')
+
+
+class AssetView(ListView):
+    template_name = 'assetsapp/assets.html'
+    context_object_name = 'assets_list'
+
+    def get_queryset(self):
+        return Asset.objects.all()
+
